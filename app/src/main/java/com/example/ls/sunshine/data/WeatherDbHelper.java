67d4c8cbfbc9c4b54f66a6a7bbcb883fc69d140d
Stas1270 <stassafyanov@gmail.com>
2015-10-22 17:05:18 +0300

    Create DB. done Test DB, add files to ContentProviders

--- app/src/main/java/com/example/ls/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/ls/sunshine/data/WeatherDbHelper.java
@@ -69,7 +69,16 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
+        final String SQL_CREATE_LOCATION_ENTRY_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                LocationEntry._ID +  " INTEGER PRIMARY KEY," +
+                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_LATITUDE + " REAL NOT NULL, " +
+                LocationEntry.COLUMN_LONGITUDE +  " REAL NOT NULL, " +
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL " +
+                ");";
+
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_ENTRY_TABLE);
     }
 
     @Override
